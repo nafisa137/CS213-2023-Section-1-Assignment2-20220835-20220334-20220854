1ea98080426d131b1bf95fba605fecacc4be5de7
Nafisa Abaalrahman <97001917+nafisa137@users.noreply.github.com>
2023-11-05 11:31:41 -0800

    Add files via upload

---
+++ A2_Task2_A_main_20220835_20220854_20220334.cpp
@@ -0,0 +1,15 @@
+//file:
+//purpose: Task 2 bigreal numbers
+//Author: Safa Tawfik, nafisa Abdulrahman, mariam matar
+//section: s1, s2
+//ID: 20220835, 20220854, 20220334
+#include "A2_Task2_A_20220835_20220854_20220334.h"
+
+using namespace std;
+int main(){
+    BigReal a("382.5481");
+    BigReal b("6562.548");
+    BigReal result = a + b;
+   cout << "Result: " << result << std::endl;
+    return 0;
+}
