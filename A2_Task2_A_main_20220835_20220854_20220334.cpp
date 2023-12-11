6841e51b49f553d6001e8c1916fb8bf88e43d14a
Nafisa Abaalrahman <97001917+nafisa137@users.noreply.github.com>
2023-12-11 19:54:57 +0200

    Update A2_Task2_A_main_20220835_20220854_20220334.cpp

--- A2_Task2_A_main_20220835_20220854_20220334.cpp
+++ A2_Task2_A_main_20220835_20220854_20220334.cpp
@@ -4,12 +4,15 @@
 //section: s1, s2
 //ID: 20220835, 20220854, 20220334
 #include "A2_Task2_A_20220835_20220854_20220334.h"
-
 using namespace std;
-int main(){
-    BigReal a("382.5481");
+int main() {
+    BigReal a("-582.5481");
     BigReal b("6562.548");
-    BigReal result = a + b;
-   cout << "Result: " << result << std::endl;
+    BigReal result("566");
+
+    if (a > b) {
+        cout << "Result: " << result << std::endl;
+    }
+
     return 0;
 }
